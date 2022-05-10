6800c96f8d6b2d8a7466762b2f95feef2ed58ead
Haitau1996 <chenht1996@gmail.com>
2022-05-10 12:25:52 +0800

    ❄️ chap 20 part II

---
+++ C++/现代C++语言核心特性/code/20-3.cxx
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+struct BindTest {
+    int a = 42;
+    std::string b = "hello structured binding";
+};
+
+int main()
+{
+    BindTest bt;
+    const auto[x, y] = bt;
+
+    std::cout << "&bt.a=" << &bt.a << " &x=" << &x << std::endl;
+    std::cout << "&bt.b=" << &bt.b << " &y=" << &y << std::endl;
+    std::cout << "std::is_same_v<const int, decltype(x)>=" 
+        << std::is_same_v<const int, decltype(x)> << std::endl;
+    std::cout << "std::is_same_v<const std::string, decltype(y)>=" 
+        << std::is_same_v<const std::string, decltype(y)> << std::endl;
+}
